d145c00fd71b6af41e49f7fbd455de721cdb414a
a-singh09 <a_singh09@yahoo.com>
2024-03-05 17:07:31 +0530

    Fixed table overflow

--- app/components/ResearchPapers.tsx
+++ app/components/ResearchPapers.tsx
@@ -47,26 +47,26 @@ export default function ResearchPapers() {
             </div>
             {/* END Heading */}
 
-            <div className=" pt-14 px-5 lg:px-40">
+            <div className=" pt-14 px-5 lg:px-20">
 
                 {/* Responsive Table Container */}
                 <div className="overflow-auto min-w-full border border-gray-200 drop-shadow-2xl-xl rounded-lg bg-white bg-opacity-25">
                     {/* Table */}
-                    <table className="min-w-full whitespace-nowrap align-middle text-sm">
+                    <table className="min-w-full whitespace-nowrap align-middle text-xs text-sm">
                         {/* Table Header */}
                         <thead>
                             <tr>
-                                <th className="px-8 py-4 text-left font-semibold bg-gray-900/25 text-gray-50">
+                                <th className="px-2 py-2 md:px-8 md:py-4 text-left font-semibold bg-gray-900/25 text-gray-50">
                                     Title
                                 </th>
 
-                                <th className="px-3 py-4 text-left font-semibold bg-gray-900/25 text-gray-50">
+                                <th className="px-1 py-2 md:px-3 md:py-4 text-left font-semibold bg-gray-900/25 text-gray-50">
                                     Authors
                                 </th>
-                                <th className="px-3 py-4 text-left font-semibold bg-gray-900/25 text-gray-50">
+                                <th className="px-1 py-2 md:px-3 md:py-4 text-left font-semibold bg-gray-900/25 text-gray-50">
                                     Year
                                 </th>
-                                <th className="px-3 py-4 text-center font-semibold bg-gray-900/25 text-gray-50">
+                                <th className="px-1 py-2 md:px-3 md:py-4 text-center font-semibold bg-gray-900/25 text-gray-50">
                                     Actions
                                 </th>
                             </tr>
@@ -77,13 +77,13 @@ export default function ResearchPapers() {
                         <tbody>
                             {articles.map((article, index) => (
                                 <tr key={index} className="even:bg-gray-50 even:bg-gray-700/25 text-gray-50">
-                                    <td className="py-3 px-8 text-left">{article.title}</td>
-                                    <td className="p-3 text-gray-400">{article.authors}</td>
-                                    <td className="p-3">{article.year}</td>
-                                    <td className="p-3 text-center">
+                                    <td className="py-2 px-2 md:py-3 md:px-8 text-left">{article.title}</td>
+                                    <td className="p-1 md:p-3 text-gray-400">{article.authors}</td>
+                                    <td className="p-1 md:p-3">{article.year}</td>
+                                    <td className="p-1 md:p-3 text-center">
                                         <button
                                             type="button"
-                                            className="inline-flex justify-center space-x-2 rounded-lg border px-2 py-1 text-sm font-semibold leading-5 hover:shadow-sm focus:ring active:shadow-none border-gray-700 bg-gray-800 text-gray-300 hover:border-gray-600 hover:text-gray-200 focus:ring-gray-600 focus:ring-opacity-40 active:border-gray-700"
+                                            className="inline-flex justify-center space-x-2 rounded-lg border px-1 py-1 md:px-2 md:py-1 text-xs text-sm font-semibold leading-5 hover:shadow-sm focus:ring active:shadow-none border-gray-700 bg-gray-800 text-gray-300 hover:border-gray-600 hover:text-gray-200 focus:ring-gray-600 focus:ring-opacity-40 active:border-gray-700"
                                             onClick={() => window.open(`${article.link}`, '_blank')}
                                         >
                                             Visit
